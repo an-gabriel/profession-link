3dc0c83c417902c5effe92308512ca1cae5fa45d
Gabriel Aquino <aquino.gabriel@outlook.com.br>
2023-08-27 15:10:11 -0300

    chore: implementation db config

--- server/src/config/db.config.ts
+++ server/src/config/db.config.ts
@@ -1,14 +1,24 @@
-import { Pool } from 'pg';
+import { Pool, PoolClient } from 'pg';
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  database: process.env.DB_NAME,
-});
+interface DatabaseConfig {
+  user: string;
+  password: string;
+  host: string;
+  port: number;
+  database: string;
+}
 
-pool.on('error', (err, client) => {
+const poolConfig: DatabaseConfig = {
+  user: process.env.DB_USER || '',
+  password: process.env.DB_PASSWORD || '',
+  host: process.env.DB_HOST || '',
+  port: Number(process.env.DB_PORT) || 5432,
+  database: process.env.DB_NAME || '',
+};
+
+const pool = new Pool(poolConfig);
+
+pool.on('error', (err: Error, client: PoolClient) => {
   console.error('Error connecting to database:', err);
 });
 
