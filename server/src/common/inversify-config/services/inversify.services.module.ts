f7880765b83287a82529c59d360c27d518a100c4
Gabriel Aquino <aquino.gabriel@outlook.com.br>
2023-08-30 03:32:44 -0300

    refactor: removing files that are no longer needed

--- server/src/common/inversify-config/services/inversify.services.module.ts
+++
@@ -1,7 +0,0 @@
-import { Container } from 'inversify';
-import { ListProfessionService } from '../../../modules/profession/services/get/list.profession.service';
-
-export function bindServices(container: Container) {
-  container.bind<ListProfessionService>(ListProfessionService).toSelf();
-  // Adicione outras vinculações de serviços aqui
-}
--- server/src/modules/profession/repository/profession.repository.ts
+++
@@ -1,8 +0,0 @@
-import { Repository } from 'typeorm';
-import { Profession } from '../entities/profession.entity';
-
-
-export class ProfessionRepository extends Repository<Profession> {
-  // Seus métodos personalizados do repositório aqui
-}
- 
