86b2c5bfafea3ae2d8c5cbe1a82f9c5cbdee9644
Gabriel Aquino <aquino.gabriel@outlook.com.br>
2023-08-30 04:51:24 -0300

    chore: add migration CreateProfessionalTable

---
+++ server/src/modules/professional/migrations/1693381594943-CreateProfessionalTable.ts
@@ -0,0 +1,91 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export class CreateProfessionalTable1693381594943
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'professional',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'nome',
+            type: 'varchar',
+            length: '255',
+            isNullable: false,
+          },
+          {
+            name: 'telefone',
+            type: 'varchar',
+            length: '20',
+            isNullable: true,
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            length: '255',
+            isNullable: true,
+          },
+          {
+            name: 'tipoDeProfissional',
+            type: 'uuid',
+            isNullable: false,
+          },
+          {
+            name: 'situacao',
+            type: 'boolean',
+            default: true,
+            isNullable: false,
+          },
+          {
+            name: 'updatedAt',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+            onUpdate: 'CURRENT_TIMESTAMP',
+            isNullable: false,
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+            isNullable: false,
+          },
+          {
+            name: 'deletedAt',
+            type: 'timestamp',
+            isNullable: true,
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'professional',
+      new TableForeignKey({
+        name: 'FK_tipoDeProfissional',
+        columnNames: ['tipoDeProfissional'],
+        referencedTableName: 'profession',
+        referencedColumnNames: ['id'],
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('professional', 'FK_tipoDeProfissional');
+    await queryRunner.dropTable('professional');
+  }
+}
