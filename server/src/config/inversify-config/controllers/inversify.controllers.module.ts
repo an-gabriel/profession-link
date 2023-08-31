4fd62f653ffe1a169c67cbdf43ba0d5bc81aabfe
Gabriel Aquino <aquino.gabriel@outlook.com.br>
2023-08-31 01:03:26 -0300

    chore: including services and controller for inversify to manage

--- server/src/config/inversify-config/controllers/inversify.controllers.module.ts
+++ server/src/config/inversify-config/controllers/inversify.controllers.module.ts
@@ -1,8 +1,10 @@
 import { Container } from 'inversify';
 import { ProfessionController } from '../../../modules/profession/controllers/profession.controller';
+import { ProfessionalController } from '../../../modules/professional/controllers/professional.controller';
 
 
 export async function bindControllers(container: Container) {
   container.bind<ProfessionController>(ProfessionController).toSelf();
+  container.bind<ProfessionalController>(ProfessionalController).toSelf();
   // Adicione outras vinculações de controladores aqui
 }
--- server/src/config/inversify-config/services/inversify.services.module.ts
+++ server/src/config/inversify-config/services/inversify.services.module.ts
@@ -1,14 +1,25 @@
 import 'reflect-metadata';
 import { Container } from 'inversify';
-import { ListProfessionService } from '../../../modules/profession/services/get/list.profession.service';
 
+import { ListProfessionService } from '../../../modules/profession/services/get/list.profession.service';
 import { UpdateProfessionService } from '../../../modules/profession/services/update/update.profession.service';
 import { DeleteProfessionService } from '../../../modules/profession/services/delete/delete.profession.service';
 import { CreateProfessionService } from '../../../modules/profession/services/create/create.profession.service';
 
+import { ListProfessionalService } from '../../../modules/professional/services/get/list.profession.service';
+import { UpdateProfessionalService } from '../../../modules/professional/services/update/update.professional.service';
+import { DeleteProfessionalService } from '../../../modules/professional/services/delete/delete.professional.service';
+import { CreateProfessionalService } from '../../../modules/professional/services/create/create.professional.service';
+
 export async function bindServices(container: Container) {
+  // profession
   container.bind<ListProfessionService>(ListProfessionService).toSelf();
   container.bind<UpdateProfessionService>(UpdateProfessionService).toSelf();
   container.bind<DeleteProfessionService>(DeleteProfessionService).toSelf();
   container.bind<CreateProfessionService>(CreateProfessionService).toSelf();
+  // professional
+  container.bind<ListProfessionalService>(ListProfessionalService).toSelf();
+  container.bind<UpdateProfessionalService>(UpdateProfessionalService).toSelf();
+  container.bind<DeleteProfessionalService>(DeleteProfessionalService).toSelf();
+  container.bind<CreateProfessionalService>(CreateProfessionalService).toSelf();
 }
