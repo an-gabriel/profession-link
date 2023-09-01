1d3cf607ac01860a960351018828908dcaf62d93
Gabriel Aquino <aquino.gabriel@outlook.com.br>
2023-09-01 00:06:41 -0300

    refactor: adjustment patterns

--- server/src/modules/professional/services/create/create.professional.service.ts
+++ server/src/modules/professional/services/create/create.professional.service.ts
@@ -1,9 +1,9 @@
 import { Connection } from 'typeorm';
 import { inject, injectable } from 'inversify';
-import { Professional } from '../../entities/professional.entity';
 import { BaseService } from '../../../../common/services/IBase.service';
 import { CreateProfessionalRequest } from '../../interfaces/professional.interface';
-import ProfessionalModel from '../../models/professional.models';
+import { Professional } from '../../entities/professional.entity';
+import { Profession } from '../../../profession/entities/profession.entity';
 
 @injectable()
 export class CreateProfessionalService extends BaseService<Professional> {
@@ -19,18 +19,25 @@ export class CreateProfessionalService extends BaseService<Professional> {
     situacao,
     telefone,
     email,
-    tipoDeProfissionalId,
+    tipoDeProfissional,
   }: CreateProfessionalRequest): Promise<Professional> {
-    const professionalModel = new ProfessionalModel(
+    const professionalRepository = await this.getRepository(Professional);
+    const professionRepository = await this.getRepository(Profession);
+
+    const profession = await professionRepository.findOne(tipoDeProfissional);
+
+    if (!profession) {
+      throw new Error('Tipo de profissional não encontrado');
+    }
+
+    const professionalEntity = professionalRepository.create({
       nome,
+      situacao,
       telefone,
       email,
-      tipoDeProfissionalId,
-      situacao,
-    );
+      tipoDeProfissional: profession.id,
+    });
 
-    const repository = await this.getRepository(Professional);
-
-    return await repository.save(professionalModel);
+    return professionalRepository.save(professionalEntity);
   }
 }
